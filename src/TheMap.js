6deab67d76eed179304587fd411aaa51e2dc310b
Clara Kang <clara.kang91@gmail.com>
2017-06-05 16:27:47 -0700

    parking routes initiated

--- src/TheMap.js
+++ src/TheMap.js
@@ -1,8 +1,9 @@
 //Map component
 import React, {Component} from 'react'
-import clientAuth from './clientAuth.js'
 import mapboxgl from 'mapbox-gl'
-import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl"
+import ReactMapboxGl, {Layer, Feature, GeoJSONLayer} from "react-mapbox-gl"
+import clientAuth from './clientAuth.js'
+import parkingsConnect from './parkingsConnect.js'
 
 class TheMap extends Component {
 /////////////////////////////////////////////////CONSTRUCTOR
@@ -14,10 +15,48 @@ class TheMap extends Component {
   }
 /////////////////////////////////////////////////LIFECYCLE
   componentDidMount() {
-
+    parkingsConnect.getParkings().then((res) => {
+      this.setState({
+        parkings: res.data
+      })
+    })
   }
 /////////////////////////////////////////////////CUSTOM FUNCTIONS
+/////////////////////////////////////////////////RENDER
   render() {
+    console.log(this.state.parkings)
+
+    const parkings = this.state.parkings.map((parking, i) => {
+      return (
+        <GeoJSONLayer
+          key = {i}
+          type = "line"
+          source = {{
+            "type": "geojson",
+            data: {
+              "type": "Feature",
+              "geometry": {
+                "type": "line",
+                "coordinates": [
+                  [34.013565, -118.495505],
+                  [34.015107, -118.493584]
+                ]
+              }
+            }
+          }}
+          layout= {{
+            'line-join': 'round',
+            'line-cap': 'round'
+          }}
+          paint ={{
+            'line-color': '#FFFFFF',
+            'line-width': 10
+          }}>
+        </GeoJSONLayer>
+      )
+    })
+
+
     return (
       <div id="map">
         <ReactMapboxGl
@@ -29,17 +68,19 @@ class TheMap extends Component {
           }}
           center={[-118.482,34.026]}
           >
+
         <Layer
           type="symbol"
           id="marker"
           layout={{ "icon-image": "marker-15" }}>
           <Feature coordinates={[-118.482,34.026]}/>
         </Layer>
+        {parkings}
         </ReactMapboxGl>
       </div>
   )
   }
-/////////////////////////////////////////////////RENDER
+
 
 }
 
--- src/parkingsConnect.js
+++ src/parkingsConnect.js
@@ -1,10 +1,15 @@
-import axios
-import jwt_decode from 'jwt-decode'
+import axios from 'axios'
+// import jwt_decode from 'jwt-decode'
 
 axios.defaults.baseURL = 'http://localhost:3001'
 
 const parkingsConnect = {
-
+  getParkings: () => {
+    return axios({
+      url: '/api/parkings',
+      method: 'get'
+    })
+  }
 }
 
 
