e0dd8d46b8a57e691d17f66005775efd5ff318cd
Clara Kang <clara.kang91@gmail.com>
2017-06-08 14:51:47 -0700

    mongo url key edited

--- src/clientAuth.js
+++ src/clientAuth.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import jwt_decode from 'jwt-decode'
 
-axios.defaults.baseURL = 'https://floating-sands-94866.herokuapp.com/'
+axios.defaults.baseURL = 'https://floating-sands-94866.herokuapp.com' 
 
 const clientAuth = {
   signup: (userInfo) => {
